01194550b4eae4d9ebfe6d00e466aa8156e0346e
Jess V <jessicacoraldrewett@gmail.com>
2025-05-25 21:51:42 +0200

    Write a script that prints x times C is fun

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,14 +1,17 @@
 const firstArgument = process.argv[2];
 const numOccurrences = parseInt(firstArgument);
+
 if (isNaN(numOccurrences)) {
     console.log("Missing number of occurrences");
 } else {
-    let outputString = ''
+    let outputString = '';
     let i = 0;
-    while (i < numOccurrences) {
+while (i < numOccurrences) {
         outputString += "C is fun\n";
-         i++;
-  }
-  const finalOutput = outputString.slice(0, -1);
-  console.log(finalOutput);
+        i++;
+    }
+    const finalOutput = outputString.slice(0, -1);
+    if (numOccurrences > 0) {
+        console.log(finalOutput);
+    }
 }
