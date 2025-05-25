3e9065246ddf36451b60110cec888a325c6735f9
Jess V <jessicacoraldrewett@gmail.com>
2025-05-25 22:00:08 +0200

    Write a script that prints the addition of 2 integers

---
+++ 9-add.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+    return a + b;
+}
+const firstArgument = process.argv[2];
+const secondArgument = process.argv[3];
+const num1 = parseInt(firstArgument);
+const num2 = parseInt(secondArgument);
+console.log(add(num1, num2));
